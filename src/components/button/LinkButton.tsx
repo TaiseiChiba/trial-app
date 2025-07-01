c7c5f36585cee152b9a3c725c3737a34016bd4f3
Taisei Chiba <nanakorobiyaoki.tai@icloud.com>
2025-07-01 22:13:23 +0900

    ボタンをコンポーネント化

---
+++ src/components/button/LinkButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import React from 'react'
+
+export default function LinkButton({ to, children }: { to: string; children: React.ReactNode }) {
+  return (
+    <Button variant="contained" component={RouterLink} to={to}>
+      {children}
+    </Button>
+  )
+}
